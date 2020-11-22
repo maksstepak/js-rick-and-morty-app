995e97158d1181c7e3175d1f77dd1f4a7f4fdc95
maksstepak <maks.stepak1@gmail.com>
2020-11-22 20:11:33 +0100

    Add babel with basic config

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,17 @@ module.exports = {
       template: path.resolve(__dirname, 'src/index.html'),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
