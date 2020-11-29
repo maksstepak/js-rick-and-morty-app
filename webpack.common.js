a4ffb82290e9014b5f6bbd47d006b496c87df9a1
maksstepak <maks.stepak1@gmail.com>
2020-11-29 13:10:43 +0100

    Update styles

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,12 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
     ],
   },
